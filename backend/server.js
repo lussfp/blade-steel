311d15f31e4a9976cf135cb769bbbd37b6fc778d
Luís Gustavo Magosso Obreli <brmagosso@gmail.com>
2026-03-12 14:36:30 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
-const cors    = require('cors');
+const cors = require('cors');
+app.use(cors()); 
 const crypto  = require('crypto');
 const path    = require('path');
 const db      = require('./db');
